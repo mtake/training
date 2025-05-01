eb76fcff8d720f3aa9200a7f411b76c1f6b44eeb
Mikio Takeuchi <mtake@jp.ibm.com>
2025-05-01 04:48:16 -0400

    update test script

--- examples-mtake/openai-test.py
+++ examples-mtake/openai-test.py
@@ -13,8 +13,8 @@ import os
 #model="meta-llama/llama-3-3-70b-instruct"
 #base_url="https://inference-3scale-apicast-production.apps.rits.fmaas.res.ibm.com/llama-3-3-70b-instruct/v1"
 
-model="microsoft/phi-4"
-base_url="https://inference-3scale-apicast-production.apps.rits.fmaas.res.ibm.com/microsoft-phi-4/v1"
+#model="microsoft/phi-4"
+#base_url="https://inference-3scale-apicast-production.apps.rits.fmaas.res.ibm.com/microsoft-phi-4/v1"
 
 #
 # NOTE: assumption
@@ -23,6 +23,30 @@ base_url="https://inference-3scale-apicast-production.apps.rits.fmaas.res.ibm.co
 #
 #model="experiments/training_output-Phi-4-mini-instruct-generic_data_teigaku-genzei/hf_format/samples_14021"
 #base_url="http://0.0.0.0:8000/v1"
+
+
+#
+# NOTE: assumption
+# pip install vllm 'flashinfer-python<0.2.3'
+# python -m vllm.entrypoints.openai.api_server --model /u/mtake/.cache/instructlab/models/granite-3.1-8b-lab-v1 --dtype auto # --api-key dummy
+#
+#model="/u/mtake/.cache/instructlab/models/granite-3.1-8b-lab-v1"
+#base_url="http://0.0.0.0:8000/v1"
+
+
+# (training-py311) [mtake@cccxc596 examples-mtake]$ ls experiments/training_output-granite-3.1-8b-lab-v1-generic_data_teigaku-genzei/hf_format | cat
+# samples_14027
+# samples_28049
+# samples_42061
+#
+# NOTE: assumption
+# pip install vllm 'flashinfer-python<0.2.3'
+# python -m vllm.entrypoints.openai.api_server --model experiments/training_output-granite-3.1-8b-lab-v1-generic_data_teigaku-genzei/hf_format/samples_14027 --dtype auto # --api-key dummy
+# python -m vllm.entrypoints.openai.api_server --model experiments/training_output-granite-3.1-8b-lab-v1-generic_data_teigaku-genzei/hf_format/samples_42061 --dtype auto # --api-key dummy
+#
+#model="experiments/training_output-granite-3.1-8b-lab-v1-generic_data_teigaku-genzei/hf_format/samples_14027"
+model="experiments/training_output-granite-3.1-8b-lab-v1-generic_data_teigaku-genzei/hf_format/samples_42061"
+base_url="http://0.0.0.0:8000/v1"
 
 #prompt="Hello!"
 #prompt="令和６年分所得税の定額減税の対象者は誰ですか？"
