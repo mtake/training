8abc25999a7739cfebdb11939f92cc54731531bf
Mikio Takeuchi <mtake@jp.ibm.com>
2025-07-30 05:52:07 -0400

    update test script

--- examples-mtake/openai-test.py
+++ examples-mtake/openai-test.py
@@ -111,8 +111,41 @@ import os
 #base_url="http://0.0.0.0:8000/v1"
 
 # vllm serve checkpoints/granite-3.3-8b-instruct-teigaku-genzei-ibm-v4-d5-interp --served-model-name granite-3.3-8b-instruct-teigaku-genzei-ibm-v4-d5-interp
-model="granite-3.3-8b-instruct-teigaku-genzei-ibm-v4-d5-interp"
+#model="granite-3.3-8b-instruct-teigaku-genzei-ibm-v4-d5-interp"
+#base_url="http://0.0.0.0:8000/v1"
+
+# vllm serve checkpoints/granite-3.3-8b-instruct-ibm-newsroom-d5 --served-model-name granite-3.3-8b-instruct-ibm-newsroom-d5
+#model="granite-3.3-8b-instruct-ibm-newsroom-d5"
+#base_url="http://0.0.0.0:8000/v1"
+
+# vllm serve checkpoints/granite-3.3-8b-instruct-ibm-newsroom-d5-interp --served-model-name granite-3.3-8b-instruct-ibm-newsroom-d5-interp
+#model="granite-3.3-8b-instruct-ibm-newsroom-d5-interp"
+#base_url="http://0.0.0.0:8000/v1"
+
+# vllm serve checkpoints/granite-3.3-8b-instruct-ibm-newsroom-d5-x100 --served-model-name granite-3.3-8b-instruct-ibm-newsroom-d5-x100
+#model="granite-3.3-8b-instruct-ibm-newsroom-d5-x100"
+#base_url="http://0.0.0.0:8000/v1"
+
+# vllm serve checkpoints/granite-3.3-8b-instruct-ibm-newsroom-d5-x100-interp --served-model-name granite-3.3-8b-instruct-ibm-newsroom-d5-x100-interp
+#model="granite-3.3-8b-instruct-ibm-newsroom-d5-x100-interp"
+#base_url="http://0.0.0.0:8000/v1"
+
+# vllm serve checkpoints/granite-3.3-8b-instruct-ibm-newsroom-en_d5 --served-model-name granite-3.3-8b-instruct-ibm-newsroom-en_d5
+#model="granite-3.3-8b-instruct-ibm-newsroom-en_d5"
+#base_url="http://0.0.0.0:8000/v1"
+
+# vllm serve checkpoints/granite-3.3-8b-instruct-ibm-newsroom-en_d5-interp --served-model-name granite-3.3-8b-instruct-ibm-newsroom-en_d5-interp
+#model="granite-3.3-8b-instruct-ibm-newsroom-en_d5-interp"
+#base_url="http://0.0.0.0:8000/v1"
+
+# vllm serve checkpoints/granite-3.3-8b-instruct-jfe-technical-report_r5 --served-model-name granite-3.3-8b-instruct-jfe-technical-report_r5
+#model="granite-3.3-8b-instruct-jfe-technical-report_r5"
+#base_url="http://0.0.0.0:8000/v1"
+
+# vllm serve checkpoints/granite-3.3-8b-instruct-jfe-technical-report_r5-interp --served-model-name granite-3.3-8b-instruct-jfe-technical-report_r5-interp
+model="granite-3.3-8b-instruct-jfe-technical-report_r5-interp"
 base_url="http://0.0.0.0:8000/v1"
+
 
 #prompt="Hello!"
 #prompt="令和６年分所得税の定額減税の対象者は誰ですか？"
@@ -121,8 +154,17 @@ base_url="http://0.0.0.0:8000/v1"
 #{"messages":[{"content":"合計所得金額が1,805万円を超える人は定額減税の対象となりますか？","role":"user"},{"content":"いいえ、合計所得金額が1,805万円を超える人は定額減税の対象とはなりません。\n\n","role":"assistant"}]}
 ##prompt="令和6年分の所得税における定額減税額とは何ですか？"
 ##{"messages":[{"content":"令和6年分の所得税における定額減税額とは何ですか？","role":"user"},{"content":"令和6年分の所得税における定額減税額は、所得税から控除できる金額で、所得者本人には3万円が控除され、同一生計配偶者や扶養親族1人につき3万円が加算されます。\n\n","role":"assistant"}]}
-prompt="令和6年分の所得税における「定額減税額」はどのように計算されますか？"
-#{"messages":[{"content":"令和6年分の所得税における「定額減税額」はどのように計算されますか？","role":"user"},{"content":"令和6年分の所得税における「定額減税額」は、所得者本人に対して3万円を基本とし、同一生計配偶者または扶養親族1人につき3万円を加算して計算されます。\n\n","role":"assistant"}]}
+##prompt="令和6年分の所得税における「定額減税額」はどのように計算されますか？"
+###{"messages":[{"content":"令和6年分の所得税における「定額減税額」はどのように計算されますか？","role":"user"},{"content":"令和6年分の所得税における「定額減税額」は、所得者本人に対して3万円を基本とし、同一生計配偶者または扶養親族1人につき3万円を加算して計算されます。\n\n","role":"assistant"}]}
+#prompt="IBM Quantum System Twoの設置を委託した、経済産業省所管の組織の名前は？"
+#prompt="IBM と理化学研究所（理研）が設置した量子コンピューターの名前は？"
+#prompt="IBM Heronプロセッサーは何量子ビットを持っていますか？"
+#prompt="「Japan–IBM Quantum Partnership」の目的は何ですか？"
+#prompt="「Japan–IBM Quantum Partnership」はいつ設立されましたか？"
+#prompt="IBM Quantum Heronプロセッサーの量子ビット数はいくつですか？"
+#prompt="Watsonx Orchestrate includes pre-built domain-specific agents. Which business areas were highlighted at IBM Think 2025?"
+prompt="JFEスチールが製造している鋼管の種類にはどのようなものがありますか?"
+#answer="JFEスチールは、継目無鋼管、各種溶接鋼管（ステンレス油井管、大径電縫鋼管マイティーシーム®、UOE鋼管、高機能高加工性電縫鋼管）などを製造しています。"
 
 messages = [{"role": "user", "content": prompt}]
 
